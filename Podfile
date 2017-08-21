e109aef014ede756d657f2f233150aa83bd3b1b1
icbc <icbc@icbcdeMacBook-Air.local>
2017-08-21 10:39:56 +0800

    init

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, ’10.0’
+
+target 'todo' do
+
+  pod ‘AMapLocation’
+  pod ‘AMap2DMap’
+  pod ‘XLForm’, ‘~> 3.0’
+  pod ‘MJRefresh’
+  
+
+  # Pods for todo
+
+end
---
+++ todo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  todo
+//
+//  Created by icbc on 2017/8/21.
+//  Copyright © 2017年 carolxfyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
