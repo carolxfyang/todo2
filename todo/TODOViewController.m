13e601f3c85d9d08839a5e371591e940fc4767dc
icbc <icbc@icbcdeMacBook-Air.local>
2017-08-21 14:24:10 +0800

    xxxx

--- todo/TODOViewController.m
+++ todo/TODOViewController.m
@@ -62,6 +62,100 @@ NSString * const kFinishedFlagTODO = @"finishedFlag";
     [self setHidesBottomBarWhenPushed:YES];
 }
 
+- (void)addTodo{
+    [self setHidesBottomBarWhenPushed:YES];
+    
+    TODOFormVC *newTodo = [[TODOFormVC alloc]initWithValues:nil];
+    newTodo.callbackDelegate = self;
+    [self.navigationController pushViewController:newTodo animated:YES];
+    
+}
+
+- (void)callback{
+    [self.tableView.mj_header beginRefreshing];
+    
+}
+
+- (void)alertWithMsg:(NSString *)msg{
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"title" message:msg preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Cancel");
+    }]];
+    [alertController addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull acion) {
+        NSLog(@"OK");
+    }]];
+    
+    [self presentViewController:alertController animated:YES completion:nil];
+    
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.listItemArray.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell;
+    NSString *idetifier = @"cell";
+    cell = [tableView dequeueReusableCellWithIdentifier:idetifier];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:idetifier];
+    }
+    NSDictionary *dict = [self.listItemArray objectAtIndex:indexPath.row];
+    if ([dict valueForKey:kFinishedFlagTODO]) {
+        cell.imageView.image = [UIImage imageNamed:@"todo_finished"];
+    }else{
+        cell.imageView.image = [UIImage imageNamed:@"todo_unfinishied"];
+    }
+    
+    cell.textLabel.text = [dict valueForKey:@"title"] == nil?@"":[dict valueForKey:@"title"];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc]init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];
+    NSString *strDate = [dateFormatter stringFromDate:[dict valueForKey:@"endTime"]];
+    cell.detailTextLabel.text = [NSString stringWithFormat:@"Due to: %@",strDate];
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    
+    return cell;
+    
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    return 10;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    return 10;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (NSArray *)tableView:(UITableView *)tableView  editActionsForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UITableViewRowAction *deleteRowAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleDestructive title:@"Delete" handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+        NSLog(@"Delete");
+    }];
+    UITableViewRowAction *doneRowAction = [UITableViewRowAction rowActionWithStyle:UITableViewRowActionStyleNormal title:@"complete" handler:^(UITableViewRowAction *action, NSIndexPath *indexPath) {
+        NSLog(@"complete");
+    }];
+    doneRowAction.backgroundColor = [UIColor colorWithRed:0 green:124/255.0 blue:223/255.0 alpha:1];
+    return @[deleteRowAction,doneRowAction];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSDictionary *data = [self.listItemArray objectAtIndex:indexPath.row];
+    
+    [self setHidesBottomBarWhenPushed:YES];
+    
+    TODOFormVC *newTodo = [[TODOFormVC alloc]initWithValues:data];
+    newTodo.callbackDelegate = self;
+    [self.navigationController pushViewController:newTodo animated:YES];
+}
 
 
 
