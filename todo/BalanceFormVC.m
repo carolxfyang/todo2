9c86a93d3596259fc170684d2381c336d8f86669
icbc <icbc@icbcdeMacBook-Air.local>
2017-08-24 16:52:55 +0800

    xxxxxxx

--- todo/BalanceFormVC.m
+++ todo/BalanceFormVC.m
@@ -82,6 +82,8 @@ NSString * const kFinishedFlagB = @"finishedFlag";
     
     if (listItemArray==nil) {
         listItemArray = [NSMutableArray arrayWithCapacity:100];
+    }else{
+        listItemArray = [[NSMutableArray alloc]initWithArray:listItemArray];
     }
     
     NSMutableDictionary *mDict = [[NSMutableDictionary alloc]initWithDictionary:values];
